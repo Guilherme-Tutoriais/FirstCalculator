8d1f3b6093cff29130656d055b502f0fdf58b6f8
Guilherme <guilhermemb11@gmail.com>
2023-06-02 22:50:07 -0300

    basic setup passing feat: onCLick

--- src/calculadora/index.tsx
+++ src/calculadora/index.tsx
@@ -1,40 +1,58 @@
-import CalcButtonNumber from "./components/CalcButton";
+import { useState } from "react";
+import CalcButtonNumber from "./components/CalcButtonNumber";
 import CalcButtonOperator from "./components/CalcButtonOperator";
 
-const Calculator = () => {
-  const handleNumber = () => {
-    console.log("number");
-  };
-  const handleOperator = () => {
-    console.log("operator");
-  };
+interface Display {
+  current: string;
+  total: string;
+  isInitial: boolean;
+}
 
+const Calculator = () => {
+  const [display, setDisplay] = useState<Display>({
+    current: "0",
+    total: "0",
+    isInitial: true,
+  });
+  const handleNumber = (value: string) => {
+    let newValue = value;
+    if (display.isInitial == false) {
+      newValue = display.current + value;
+    }
+    setDisplay({ current: newValue, total: display.total, isInitial: false });
+  };
+  const handleOperator = (op: string) => {
+    console.log(`operation: ${op}`);
+  };
+  const renderDisplay = () => {
+    return display.current;
+  };
   return (
     <>
-      <p>calculadora</p>
+      <div>{renderDisplay()}</div>
       <div>
-        <CalcButtonNumber label="7" />
-        <CalcButtonNumber label="8" />
-        <CalcButtonNumber label="9" />
-        <CalcButtonOperator label="*" />
+        <CalcButtonNumber value="7" onClick={handleNumber} />
+        <CalcButtonNumber value="8" onClick={handleNumber} />
+        <CalcButtonNumber value="9" onClick={handleNumber} />
+        <CalcButtonOperator value="*" onClick={handleOperator} />
       </div>
       <div>
-        <CalcButtonNumber label="4" />
-        <CalcButtonNumber label="5" />
-        <CalcButtonNumber label="6" />
-        <CalcButtonOperator label="-/" />
+        <CalcButtonNumber value="4" onClick={handleNumber} />
+        <CalcButtonNumber value="5" onClick={handleNumber} />
+        <CalcButtonNumber value="6" onClick={handleNumber} />
+        <CalcButtonOperator value="/" onClick={handleOperator} />
       </div>
       <div>
-        <CalcButtonNumber label="1" />
-        <CalcButtonNumber label="2" />
-        <CalcButtonNumber label="3" />
-        <CalcButtonOperator label="-" />
+        <CalcButtonNumber value="1" onClick={handleNumber} />
+        <CalcButtonNumber value="2" onClick={handleNumber} />
+        <CalcButtonNumber value="3" onClick={handleNumber} />
+        <CalcButtonOperator value="-" onClick={handleOperator} />
       </div>
       <div>
-        <CalcButtonOperator label="C" />
-        <CalcButtonNumber label="0" />
-        <CalcButtonOperator label="=" />
-        <CalcButtonOperator label="+" />
+        <CalcButtonOperator value="C" onClick={handleOperator} />
+        <CalcButtonNumber value="0" onClick={handleNumber} />
+        <CalcButtonOperator value="=" onClick={handleOperator} />
+        <CalcButtonOperator value="+" onClick={handleOperator} />
       </div>
     </>
   );
